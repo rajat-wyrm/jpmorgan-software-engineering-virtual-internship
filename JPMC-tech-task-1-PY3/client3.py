b9f19fe53c02443a9a8fc090641b058e71bb3002
rajat-wyrm <rajat-wyrm@users.noreply.github.com>
2025-12-11 20:01:01 +0530

    chore(task1): complete client3.py

--- JPMC-tech-task-1-PY3/client3.py
+++ JPMC-tech-task-1-PY3/client3.py
@@ -40,3 +40,25 @@ def getDataPoint(quote):
 
 def getRatio(price_a, price_b):
 	""" Get ratio of price_a and price_b """
+	""" ------------- Update this function ------------- """
+	""" Also create some unit tests for this function in client_test.py """
+	if (price_b == 0):
+			#when price_b is zero avoid throwing ZeroDevisionError
+			return 
+	return price_a/price_b
+
+# Main
+if __name__ == "__main__":
+
+	# Query the price once every N seconds.
+	for _ in range(N):
+		quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
+
+		""" ----------- Update to get the ratio --------------- """
+		prices = {}
+		for quote in quotes:
+			stock, bid_price, ask_price, price = getDataPoint(quote)
+			prices[stock] = price
+			print ("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+
+		print ("Ratio %s" % (getRatio(prices['ABC'], prices['DEF'])))
