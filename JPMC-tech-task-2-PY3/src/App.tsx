c857e35eee82413d2a9874e7bb58efa16f332251
rajat-wyrm <rajat-wyrm@users.noreply.github.com>
2026-01-06 22:22:08 +0530

    feat(task2-src): extend App.tsx (chunk 2/3)

--- JPMC-tech-task-2-PY3/src/App.tsx
+++ JPMC-tech-task-2-PY3/src/App.tsx
@@ -27,3 +27,32 @@ class App extends Component<{}, IState> {
     };
   }
 
+  /**
+   * Render Graph react component with state.data parse as property data
+   */
+  renderGraph() {
+    if(this.state.ShowGraph) {
+      return (<Graph data={this.state.data}/>)
+    }
+  }
+  /**
+   * Get new data from server and update the state with the new data
+   */
+  getDataFromServer() {
+    let x = 0;
+      const interval = setInterval(() => {
+        DataStreamer.getData((serverResponds: ServerRespond[]) => {
+          // Update the state by creating a new array of data that consists of
+          // Previous data in the state and the new data from server
+          this.setState({ 
+            data: serverResponds,
+            ShowGraph: true,
+          });
+        });
+        x++;
+        if ( x > 1000 ) {
+          clearInterval(interval);
+        }
+      }, 100);
+  }
+
