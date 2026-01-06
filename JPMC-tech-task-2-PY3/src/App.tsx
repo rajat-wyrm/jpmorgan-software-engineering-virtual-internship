91c6f56a20f941176753fe6ea3effad5ea42685d
rajat-wyrm <rajat-wyrm@users.noreply.github.com>
2026-01-06 12:34:18 +0530

    feat(task2-src): scaffold App.tsx

---
+++ JPMC-tech-task-2-PY3/src/App.tsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import DataStreamer, { ServerRespond } from './DataStreamer';
+import Graph from './Graph';
+import './App.css';
+
+/**
+ * State declaration for <App />
+ */
+interface IState {
+  data: ServerRespond[],
+  ShowGraph: boolean,
+}
+
+/**
+ * The parent element of the react app.
+ * It renders title, button and Graph react element.
+ */
+class App extends Component<{}, IState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      // data saves the server responds.
+      // We use this state to parse data down to the child element (Graph) as element property
+      data: [],
+      ShowGraph: false,
+    };
+  }
+
