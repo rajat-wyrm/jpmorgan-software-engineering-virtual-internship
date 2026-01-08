1ba61d486f16bc785088c8bf0d0f8a19c1c00dc9
rajat-wyrm <rajat-wyrm@users.noreply.github.com>
2026-01-08 11:40:00 +0530

    feat(task2-src): complete App.tsx

--- JPMC-tech-task-2-PY3/src/App.tsx
+++ JPMC-tech-task-2-PY3/src/App.tsx
@@ -56,3 +56,32 @@ class App extends Component<{}, IState> {
       }, 100);
   }
 
+  /**
+   * Render the App react component
+   */
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Bank & Merge Co Task 2
+        </header>
+        <div className="App-content">
+          <button className="btn btn-primary Stream-button"
+            // when button is click, our react app tries to request
+            // new data from the server.
+            // As part of your task, update the getDataFromServer() function
+            // to keep requesting the data every 100ms until the app is closed
+            // or the server does not return anymore data.
+            onClick={() => {this.getDataFromServer()}}>
+            Start Streaming Data
+          </button>
+          <div className="Graph">
+            {this.renderGraph()}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
