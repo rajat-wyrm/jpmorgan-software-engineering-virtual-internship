a0f8f26fd6b2d742ca9d4b21fffa31b1aff0940f
rajat-wyrm <rajat-wyrm@users.noreply.github.com>
2026-01-08 19:41:30 +0530

    feat(task2-datafeed): extend server3.py (chunk 2/5)

--- JPMC-tech-task-2-PY3/datafeed/server3.py
+++ JPMC-tech-task-2-PY3/datafeed/server3.py
@@ -62,3 +62,67 @@ OVERLAP = 4
 def bwalk(min, max, std):
     """ Generates a bounded random walk. """
     rng = max - min
+    while True:
+        max += normalvariate(0, std)
+        yield abs((max % (rng * 2)) - rng) + min
+
+def market(t0 = MARKET_OPEN):
+    """ Generates a random series of market conditions,
+        (time, price, spread).
+    """
+    for hours, px, spd in zip(bwalk(*FREQ), bwalk(*PX), bwalk(*SPD)):
+        yield t0, px, spd
+        t0 += timedelta(hours = abs(hours))
+
+def orders(hist):
+    """ Generates a random set of limit orders (time, side, price, size) from
+        a series of market conditions.
+    """
+    for t, px, spd in hist:
+        stock = 'ABC' if random() > 0.5 else 'DEF'
+        side, d  = ('sell', 2) if random() > 0.5 else ('buy', -2)
+        order = round(normalvariate(px + (spd / d), spd / OVERLAP), 2)
+        size  = int(abs(normalvariate(0, 100)))
+        yield t, stock, side, order, size
+
+
+################################################################################
+#
+# Order Book
+
+def add_book(book, order, size, _age = 10):
+    """ Add a new order and size to a book, and age the rest of the book. """
+    yield order, size, _age
+    for o, s, age in book:
+        if age > 0:
+            yield o, s, age - 1
+
+def clear_order(order, size, book, op = operator.ge, _notional = 0):
+    """ Try to clear a sized order against a book, returning a tuple of
+        (notional, new_book) if successful, and None if not.  _notional is a
+        recursive accumulator and should not be provided by the caller.
+    """
+    (top_order, top_size, age), tail = book[0], book[1:]
+    if op(order, top_order):
+        _notional += min(size, top_size) * top_order
+        sdiff = top_size - size
+        if sdiff > 0:
+            return _notional, list(add_book(tail, top_order, sdiff, age))
+        elif len(tail) > 0:
+            return clear_order(order, -sdiff, tail, op, _notional)
+
+def clear_book(buy = None, sell = None):
+    """ Clears all crossed orders from a buy and sell book, returning the new
+        books uncrossed.
+    """
+    while buy and sell:
+        order, size, _ = buy[0]
+        new_book = clear_order(order, size, sell)
+        if new_book:
+            sell = new_book[1]
+            buy  = buy[1:]
+        else:
+            break
+    return buy, sell
+
+def order_book(orders, book, stock_name):
