1e5d68b4700f44430f414615352f4d63b2a918c5
rajat-wyrm <rajat-wyrm@users.noreply.github.com>
2026-01-13 19:04:24 +0530

    feat(task2-src): complete index.tsx

--- JPMC-tech-task-2-PY3/src/index.tsx
+++ JPMC-tech-task-2-PY3/src/index.tsx
@@ -5,3 +5,10 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 declare global {
+  interface Window { perspective: any; }
+}
+
+/**
+ * React App attaches the DOM to the DOM element #root in public/index.html
+ */
+ReactDOM.render(<App />, document.getElementById('root'));
