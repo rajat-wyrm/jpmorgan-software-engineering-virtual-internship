071972dfd04385c6e07674dd29aea96246f4e3fd
rajat-wyrm <rajat-wyrm@users.noreply.github.com>
2026-01-15 16:43:52 +0530

    feat(task2-src): scaffold Graph.tsx

---
+++ JPMC-tech-task-2-PY3/src/Graph.tsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Table } from '@jpmorganchase/perspective';
+import { ServerRespond } from './DataStreamer';
+import './Graph.css';
+
+/**
+ * Props declaration for <Graph />
+ */
+interface IProps {
+  data: ServerRespond[],
+}
+
+/**
+ * Perspective library adds load to HTMLElement prototype.
+ * This interface acts as a wrapper for Typescript compiler.
+ */
+interface PerspectiveViewerElement extends HTMLElement {
+  load: (table: Table) => void,
+}
+
+/**
+ * React component that renders Perspective based on data
+ * parsed from its parent through data property.
+ */
+class Graph extends Component<IProps, {}> {
+  // Perspective table
+  table: Table | undefined;
