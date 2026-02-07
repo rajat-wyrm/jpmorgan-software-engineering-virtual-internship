6a6947686005e6df307635f372f30cf5c6e51e29
rajat-wyrm <rajat-wyrm@users.noreply.github.com>
2026-02-07 19:21:37 +0530

    feat(task3-src): scaffold DataManipulator.ts

---
+++ JPMC-tech-task-3-PY3/src/DataManipulator.ts
@@ -0,0 +1,16 @@
+import { ServerRespond } from './DataStreamer';
+
+export interface Row {
+  price_abc: number,
+  price_def: number,
+  ratio: number,
+  timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
+  
+}
+
+export class DataManipulator {
+  static generateRow(serverResponds: ServerRespond[]): Row {
+    const priceABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price)/2;
