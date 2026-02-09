691ce25521619fc92d0a1ec021562e252d4c1171
rajat-wyrm <rajat-wyrm@users.noreply.github.com>
2026-02-09 18:39:26 +0530

    feat(task3-src): complete DataManipulator.ts

--- JPMC-tech-task-3-PY3/src/DataManipulator.ts
+++ JPMC-tech-task-3-PY3/src/DataManipulator.ts
@@ -14,3 +14,20 @@ export interface Row {
 export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]): Row {
     const priceABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price)/2;
+    const priceDEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price)/2;
+    const ratio = priceABC / priceDEF;
+    const upper_bound = 1 + 0.01;
+    const lower_bound = 1 - 0.01;
+    return {
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio,
+      timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ?
+                  serverResponds[0].timestamp : serverResponds[1].timestamp,
+      upper_bound: upper_bound,
+      lower_bound: lower_bound,
+      trigger_alert: (ratio > upper_bound || ratio < lower_bound ) ? ratio : undefined,
+
+      };
+  }
+}
