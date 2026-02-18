3779f86e8db512ee4dc8cb4e2ecaad33bea7b307
rajat-wyrm <rajat-wyrm@users.noreply.github.com>
2026-02-18 16:40:23 +0530

    feat(task3-src): extend App.tsx (chunk 2/3)

--- JPMC-tech-task-3-PY3/src/App.tsx
+++ JPMC-tech-task-3-PY3/src/App.tsx
@@ -17,3 +17,23 @@ class App extends Component<{}, IState> {
     };
   }
 
+  renderGraph() {
+    if (this.state.showGraph) {
+      return (<Graph data={this.state.data}/>)
+    }
+  }
+
+  getDataFromServer() {
+    let x = 0;
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        this.setState({
+          data: serverResponds,
+          showGraph: true,
+        });
+      });
+      x++;
+      if (x > 1000) {
+        clearInterval(interval);
+      }
+    }, 100);
