64b27929a2808410ab7d58f2e200b6a8aeb15d38
rajat-wyrm <rajat-wyrm@users.noreply.github.com>
2026-03-04 13:38:30 +0530

    feat(task3-src): scaffold Graph.tsx

---
+++ JPMC-tech-task-3-PY3/src/Graph.tsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Table } from '@jpmorganchase/perspective';
+import { ServerRespond } from './DataStreamer';
+import { DataManipulator } from './DataManipulator';
+import './Graph.css';
+
+interface IProps {
+  data: ServerRespond[],
+}
+
+interface PerspectiveViewerElement extends HTMLElement {
+  load: (table: Table) => void,
+}
+class Graph extends Component<IProps, {}> {
+  table: Table | undefined;
+
+  render() {
+    return React.createElement('perspective-viewer');
+  }
+
+  componentDidMount() {
