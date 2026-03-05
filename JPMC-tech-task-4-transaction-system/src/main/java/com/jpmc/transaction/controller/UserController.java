e398b31a134a128dcf0289870bd0bcb073e2fcac
rajat-wyrm <rajatkumar7861813@gmail.com>
2026-03-05 22:17:31 +0530

    task4: Add REST controller and app config

---
+++ JPMC-tech-task-4-transaction-system/src/main/java/com/jpmc/transaction/controller/UserController.java
@@ -0,0 +1,22 @@
+﻿package com.jpmc.transaction.controller;
+
+import com.jpmc.transaction.dto.BalanceResponse;
+import com.jpmc.transaction.service.TransactionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+    
+    private final TransactionService transactionService;
+    
+    @GetMapping("/{userId}/balance")
+    public ResponseEntity<BalanceResponse> getUserBalance(@PathVariable Long userId) {
+        BigDecimal balance = transactionService.getUserBalance(userId);
+        return ResponseEntity.ok(new BalanceResponse(userId, balance));
+    }
+}
