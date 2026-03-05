4cb0f0abcb9a70c4619f7628ed1619f790424354
rajat-wyrm <rajatkumar7861813@gmail.com>
2026-03-05 22:17:38 +0530

    task4: Add test class

---
+++ JPMC-tech-task-4-transaction-system/src/test/java/com/jpmc/transaction/TransactionApplicationTests.java
@@ -0,0 +1,27 @@
+﻿package com.jpmc.transaction;
+
+import com.jpmc.transaction.model.User;
+import com.jpmc.transaction.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import java.math.BigDecimal;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TransactionApplicationTests {
+    
+    @Autowired
+    private TestRestTemplate restTemplate;
+    
+    @Autowired
+    private UserRepository userRepository;
+    
+    @Test
+    public void contextLoads() {
+        assertThat(restTemplate).isNotNull();
+        assertThat(userRepository).isNotNull();
+    }
+}
