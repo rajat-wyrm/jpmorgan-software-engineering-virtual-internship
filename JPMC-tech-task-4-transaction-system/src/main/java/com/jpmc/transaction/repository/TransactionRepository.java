b9ceb5121233959fd66aab648f4c379ef2209444
rajat-wyrm <rajatkumar7861813@gmail.com>
2026-03-05 22:16:47 +0530

    task4: Add repository interfaces

---
+++ JPMC-tech-task-4-transaction-system/src/main/java/com/jpmc/transaction/repository/TransactionRepository.java
@@ -0,0 +1,14 @@
+﻿package com.jpmc.transaction.repository;
+
+import com.jpmc.transaction.model.Transaction;
+import com.jpmc.transaction.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findByUser(User user);
+    List<Transaction> findByUserId(Long userId);
+    List<Transaction> findByStatus(String status);
+}
