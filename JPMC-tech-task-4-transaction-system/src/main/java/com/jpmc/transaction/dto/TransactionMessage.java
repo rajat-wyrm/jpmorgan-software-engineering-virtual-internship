7cb0d914c89c67b1d918a6199846e04dd0e16595
rajat-wyrm <rajatkumar7861813@gmail.com>
2026-03-05 22:17:01 +0530

    task4: Add DTO classes

---
+++ JPMC-tech-task-4-transaction-system/src/main/java/com/jpmc/transaction/dto/TransactionMessage.java
@@ -0,0 +1,12 @@
+﻿package com.jpmc.transaction.dto;
+
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Data
+public class TransactionMessage {
+    private String transactionId;
+    private Long userId;
+    private BigDecimal amount;
+    private String type;
+}
