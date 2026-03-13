6ed3f481819ffd5050b6c4a4296c053abc1d0b34
rajat-wyrm <rajat-wyrm@users.noreply.github.com>
2026-03-13 19:33:38 +0530

    feat(task3-src): complete index.tsx

--- JPMC-tech-task-3-PY3/src/index.tsx
+++ JPMC-tech-task-3-PY3/src/index.tsx
@@ -3,3 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
+
+declare global {
+  interface Window { perspective: any; }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
